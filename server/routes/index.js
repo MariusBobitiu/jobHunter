375bdd2f28d7d1cd3a1474e6fffd831a2b421095
Xendorr <79709918+Xendorr@users.noreply.github.com>
2024-03-19 22:57:02 +0000

    remove unnecessary files

--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const userRouter = require("./userRouter");
-
-router.use("/auth", userRouter);
-
-module.exports = router;
--- server/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const UserController = require("../controllers/UserController");
-
-router.post("/register", UserController.register);
-router.post("/login", UserController.login);
-router.get("/refresh", UserController.refresh);
-
-module.exports = router;
